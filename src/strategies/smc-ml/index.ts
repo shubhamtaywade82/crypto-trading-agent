a24a0a0232256886bb35a877766e9e42f0ba2b7e
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-09-25 07:18:44 +0530

    feat(smc): export SMC ML strategy surface

---
+++ src/strategies/smc-ml/index.ts
@@ -0,0 +1,5 @@
+export { SmcMlRuntime, DEFAULT_SMC_RUNTIME_OPTIONS } from './SmcMlRuntime.js';
+export { SmcExecutionAdvisor } from './SmcExecutionAdvisor.js';
+export { analyzeSmcFrame, analyzeSmcMultiTimeframe } from './SmcMlEngine.js';
+export { buildSmcConfluence, scoreFrame } from './SmcConfluence.js';
+export * from './types.js';

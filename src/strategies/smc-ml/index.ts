4efcbf8c9e86eb045817c2823f97981ae45052f3
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-09-25 09:02:34 +0530

    feat: expose SMC lifecycle runner trigger

--- src/strategies/smc-ml/index.ts
+++ src/strategies/smc-ml/index.ts
@@ -1,4 +1,4 @@
-export { SmcMlRuntime, DEFAULT_SMC_RUNTIME_OPTIONS } from './SmcMlRuntime.js';
+export { SmcMlRuntime, DEFAULT_SMC_RUNTIME_OPTIONS, buildExecutionCandidates } from './SmcMlRuntime.js';
 export { SmcExecutionAdvisor } from './SmcExecutionAdvisor.js';
 export { analyzeSmcFrame, analyzeSmcMultiTimeframe } from './SmcMlEngine.js';
 export { buildSmcConfluence, scoreFrame } from './SmcConfluence.js';
@@ -20,6 +20,7 @@ export type {
 } from './SmcTradeLifecycle.js';
 
 export { BinanceSmcLifecycleExchange } from './SmcBinanceLifecycleExchange.js';
+export { SmcMlRunner, smcRunnerLockKey, parseMarkPriceEvent } from './SmcMlRunner.js';
 export {
   SmcTradeLifecycleCoordinator,
   smcLifecycleTransitionId,
